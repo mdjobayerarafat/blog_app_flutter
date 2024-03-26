5da45da64e1eb878b32291805caf48fb77c92a05
Jubayer Arafat <mdjobayerarafat@gmail.com>
2024-03-26 21:14:13 +0600

    1

---
+++ blog_app/lib/features/auth/presentation/pages/sinup_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SinUpPagState extends StatefulWidget {
+  const SinUpPagState({super.key});
+
+  @override
+  State<SinUpPagState> createState() => SinUpPagStateState();
+}
+
+class SinUpPagStateState extends State<SinUpPagState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
